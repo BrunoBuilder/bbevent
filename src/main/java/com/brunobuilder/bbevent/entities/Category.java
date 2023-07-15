74fa06f148b9b0b27fd9346e0b85f6128bf44a4f
BrunoBuilder <brunofernandotadeu@hotmail.com>
2023-07-15 11:21:51 -0300

    tb_category created

---
+++ src/main/java/com/brunobuilder/bbevent/entities/Category.java
@@ -0,0 +1,37 @@
+package com.brunobuilder.bbevent.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
