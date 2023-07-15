841e70407d7dc2f99c4766ba6dda7f6e40c3825d
BrunoBuilder <brunofernandotadeu@hotmail.com>
2023-07-15 11:01:16 -0300

    First commit: Maven project created

---
+++ src/main/java/com/brunobuilder/bbevent/BbeventApplication.java
@@ -0,0 +1,13 @@
+package com.brunobuilder.bbevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbeventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunobuilder/bbevent/BbeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunobuilder.bbevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
